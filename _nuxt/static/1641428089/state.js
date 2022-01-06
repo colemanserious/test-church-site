6ea4d1a3a6ea360ef82df3f323a0be04b372773a
colemanserious <colemanserious@users.noreply.github.com>
2022-01-06 00:16:32 +0000

    deploy: 164f832e5a700d1cc34a97fff4803ad5055ea9c5

--- _nuxt/static/1641428089/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641428089",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"03714460"}}}}(null,"\u002F"));
