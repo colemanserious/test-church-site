2a4b92a34536aefbd70755d807e84fb6eeb33f09
colemanserious <colemanserious@users.noreply.github.com>
2022-01-06 01:53:21 +0000

    deploy: 2e8c50395e2dd2ae3c1d253826efe907760ea719

---
+++ _nuxt/static/1641433999/services/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/services", {data:[{services:[{slug:"2022-01-02",title:"January 2, 2022"},{slug:"2022-01-03",title:"January 3, 2022"},{slug:"2022-01-04",title:"January 4, 2022"}]}],fetch:{},mutations:void 0});
