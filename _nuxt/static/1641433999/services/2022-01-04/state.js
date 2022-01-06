26cee69539e3df9d709617d9b05b50fffd0eef66
colemanserious <colemanserious@users.noreply.github.com>
2022-01-06 01:56:22 +0000

    deploy: 3262af5dc48474699ec4fbf1afadec692a69b0d6

--- _nuxt/static/1641433999/services/2022-01-04/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641433999",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices\u002F2022-01-04",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e315c1c6"}}}}(null));
---
+++ _nuxt/static/1641434180/services/2022-01-02/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641434180",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices\u002F2022-01-02",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"780cea04"}}}}(null));
